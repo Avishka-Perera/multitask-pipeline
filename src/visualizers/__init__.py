95851ec11cc7d14214b53decdd28b8807a9ae320
Avishka Perera <pereramat2000@gmail.com>
2024-02-19 08:52:53 +0530

    updated BaseVisualizer

--- src/visualizers/__init__.py
+++ src/visualizers/__init__.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 
 
 class BaseVisualizer:
-    def __init__(self, name: str, max_imgs_per_batch: int = None) -> None:
-        self.name = name
+    def __init__(self, name: str = None, max_imgs_per_batch: int = None) -> None:
+        self.name = self.__class__.__name__ if name is None else name
         self.writer: SummaryWriter = None
         self.max_imgs_per_batch = max_imgs_per_batch
         self.global_step = 0
@@ -39,13 +39,13 @@ class BaseVisualizer:
     ) -> None:
         raise NotImplementedError()
 
-    def _output(self, visualization: np.ndarray, loop: str) -> None:
+    def _output(self, visualization: np.ndarray, loop: str = None) -> None:
         if self.writer is None:
             plt.imshow(visualization)
             plt.show()
         else:
             self.writer.add_images(
-                f"{self.name}/{loop}",
+                self.name if loop is None else f"{self.name}/{loop}",
                 visualization,
                 self.global_step,
                 dataformats="HWC",
