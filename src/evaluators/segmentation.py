5ff9f8ca633436db84dc115b74a8eef1d46e407d
muditha11 <muditha11d@gmail.com>
2024-03-09 11:05:08 +0530

    segmentation evaluator adapted

--- src/evaluators/segmentation.py
+++ src/evaluators/segmentation.py
@@ -32,6 +32,66 @@ class SegmentationEvaluator(BaseEvaluator):
         rgb_image = rgba_image[:, :, :3]
         return rgb_image
 
+    def process_batch(
+        self, batch: Dict[str, torch.Tensor], info: Dict[str, torch.Tensor]
+    ) -> None:
+        logits = info["logits"].cpu().detach().numpy() #80 128 128
+        # logits = logits.argmax(axis=1)
+        labels = batch["seg"].cpu().detach().numpy() #128 128
+        images = batch[self.batch_img_key].cpu().detach().numpy()
+        labels = labels.squeeze()
+        total_intersec, total_union = 0, 0
+
+        # labels_mask = np.zeros(labels.shape)
+        # labels_mask[labels > 0.5 ] = 1
+        # logits_mask = np.zeros(logits.shape)
+        # logits_mask[logits > 0.5] = 1
+        # intersection_mask = np.logical_and(labels_mask, logits_mask)
+        # union_mask = np.logical_or(labels_mask, logits_mask)
+        # intersect = intersection_mask.sum()
+        # union = union_mask.sum()
+        # total_intersec += intersect
+        # total_union += union
+
+        for j in range(logits.shape[0]):
+            logits_mask = np.zeros(logits[j].shape)
+            logits_mask[logits[j] > 0.5] = 1
+            labels_ = labels[j]
+            for i in np.unique(labels_):
+                i = int(i)
+                mask_1 = np.zeros_like(labels_)
+                mask_1[labels_==i] = 1
+                intersection_mask = np.logical_and(logits_mask[i], mask_1)
+                union_mask = np.logical_or(logits_mask[i], mask_1)
+
+                intersect = intersection_mask.sum()
+                union = union_mask.sum()
+                total_intersec += intersect
+                total_union += union
+
+
+        imgs = []
+        for i in range(len(images)):
+            img = self._visualize_segmentation(
+                images[i], labels[i], logits[i], self.seg_id
+            )
+            imgs.append(img)
+
+        return {"intersec": total_intersec, "union": total_union, "imgs": imgs}
+    
+    # def __visualize_segmentation(self, img, seg_lbl, seg_prd , seg_id):  #seg_lbl 128, 128   img 3, 128, 128 #seg_prd 80 128 128
+    #     import matplotlib.pyplot as plt
+    #     class_indices = np.unique(seg_prd)
+    #     cmap = plt.cm.get_cmap('tab20', len(class_indices))
+    #     color_map = cmap(np.arange(len(class_indices)))
+
+    #     rgb_image = np.zeros((*seg_prd.shape, 3))
+    #     for i, class_index in enumerate(class_indices):
+    #         if class_index != 0:
+    #             rgb_image[seg_prd == class_index] = color_map[i][:3] 
+    #     conc = np.concatenate((img.transpose(1,2,0), rgb_image), 1)
+    #     return conc 
+
     # def __visualize_segmentation(self, img, seg_prd, seg_lbl, seg_id): #seg_lbl 128, 128   img 3, 128, 128
     #     gt_binary_mask = np.zeros_like(seg_lbl)
     #     gt_binary_mask[seg_lbl == seg_id] = 1
@@ -64,50 +124,14 @@ class SegmentationEvaluator(BaseEvaluator):
     #     img = (img * 255).astype(np.uint8)
 
     #     return img
-
-    def process_batch(
-        self, batch: Dict[str, torch.Tensor], info: Dict[str, torch.Tensor]
-    ) -> None:
-        logits = info["logits"].cpu().detach().numpy()
-        logits = logits.argmax(axis=1)
-        labels = batch["seg"].cpu().detach().numpy()
-        images = batch[self.batch_img_key].cpu().detach().numpy()
-        labels = labels.squeeze()
-        total_intersec, total_union = 0, 0
-
-        labels_mask = np.zeros(labels.shape)
-        labels_mask[labels > 0.5 ] = 1
-        logits_mask = np.zeros(labels.shape)
-        logits_mask[logits > 0.5] = 1
-        intersection_mask = np.logical_and(labels_mask, logits_mask)
-        union_mask = np.logical_or(labels_mask, logits_mask)
-        intersect = intersection_mask.sum()
-        union = union_mask.sum()
-        total_intersec += intersect
-        total_union += union
-
-        imgs = []
-        for i in range(len(images)):
-            img = self._visualize_segmentation(
-                images[i], labels[i], logits[i], self.seg_id
-            )
-            imgs.append(img)
-
-        return {"intersec": total_intersec, "union": total_union, "imgs": imgs}
     
-    def _visualize_segmentation(self, img,seg_lbl, segmentation_maps , seg_id):  #seg_lbl 128, 128   img 3, 128, 128
-        import matplotlib.pyplot as plt
-        
-        class_indices = np.unique(segmentation_maps)
-        cmap = plt.cm.get_cmap('tab20', len(class_indices))
-        color_map = cmap(np.arange(len(class_indices)))
-
-        rgb_image = np.zeros((*segmentation_maps.shape, 3))
-        for i, class_index in enumerate(class_indices):
-            if class_index != 0:
-                rgb_image[segmentation_maps == class_index] = color_map[i][:3] 
-        conc = np.concatenate((img.transpose(1,2,0), rgb_image), 1)
-        return conc 
+    def _visualize_segmentation(self, img, seg_lbl, seg_prd, seg_id):
+        seg_prd = np.asarray(seg_prd)
+        colormap = np.random.rand(80, 3)
+        masked_clormap = np.ones((*seg_prd.shape, 3)) * colormap[:, None, None] * seg_prd[:,:,:,None]  #128,128,3 x 3 x 
+        img1 = masked_clormap.sum(axis=(0))
+        conc = np.concatenate([img.transpose(1, 2, 0), img1], 1)
+        return conc
 
     def _save_report(self, iou: float) -> str:
         report = f"IoU: {iou}"
